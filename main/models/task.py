cd40552a2efeddc9e3c20054c3b16f1f8ed19353
Kirill Gorshkov <kirill.gorshkov@dualbootpartners.com>
2022-08-24 11:07:54 +0400

    Added tests

--- main/models/task.py
+++ main/models/task.py
@@ -9,19 +9,19 @@ class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=512)
     status = models.CharField(choices=Status.choices, max_length=20)
-    priority = models.IntegerField()
+    priority = models.IntegerField(null=True)
 
     created_at = models.DateTimeField(default=timezone.now)
-    start_at = models.DateTimeField()
-    finish_until = models.DateTimeField()
+    start_at = models.DateTimeField(null=True)
+    finish_until = models.DateTimeField(null=True)
 
     tag = models.ManyToManyField(Tag)
 
     author = models.ForeignKey(
-        "User", related_name="authored_tasks", on_delete=models.DO_NOTHING
+        "User", related_name="authored_tasks", on_delete=models.DO_NOTHING, null=True
     )
     assignee = models.ForeignKey(
-        "User", related_name="assigned_tasks", on_delete=models.DO_NOTHING
+        "User", related_name="assigned_tasks", on_delete=models.DO_NOTHING, null=True
     )
 
     def __str__(self):
